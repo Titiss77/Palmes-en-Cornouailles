ca62af6f093f536bc57c87f23995fa946ea872d0
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-24 16:58:59 +0200

    Update l_global.php

--- palmes-en-cornouaille.fr/htdocs/app/Views/Public/Layout/l_global.php
+++ palmes-en-cornouaille.fr/htdocs/app/Views/Public/Layout/l_global.php
@@ -20,6 +20,22 @@ $menuItems = [
         href="<?= base_url('Assets/css/Public/global.css?v=' . filemtime(FCPATH . 'Assets/css/Public/global.css')); ?>">
     <link rel="stylesheet"
         href="<?= base_url('Assets/css/' . $cssPage . '?v=' . filemtime(FCPATH . 'Assets/css/' . $cssPage)); ?>">
+
+    <!-- Open Graph / Facebook / LinkedIn -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?= base_url() ?>">
+    <meta property="og:title" content="Palmes en Cornouaille">
+    <meta property="og:description"
+        content="Bienvenue au Palmes en Cornouaille. Nous sommes ravis de vous accueillir sur le site officiel de notre club de natation basé à Quimper.">
+    <meta property="og:image" content="<?= base_url('favicon.png') ?>">
+
+    <!-- Twitter Cards -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:url" content="<?= base_url() ?>">
+    <meta name="twitter:title" content="Palmes en Cornouaille">
+    <meta name="twitter:description"
+        content="Bienvenue au Palmes en Cornouaille. Nous sommes ravis de vous accueillir sur le site officiel de notre club de natation basé à Quimper.">
+    <meta name="twitter:image" content="<?= base_url('favicon.png') ?>">
 </head>
 <nav>
     <div class="nav-brand">
